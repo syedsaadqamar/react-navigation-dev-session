48fc6a55c8734a68701810a7e4ab25b69d3e8807
Saad <syed.saad@iomechs.com>
2020-06-26 00:18:13 +0500

    Implement 1.1 part

--- App.js
+++ App.js
@@ -1,14 +1,41 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { StatusBar } from 'expo-status-bar';
 import * as React from 'react';
-import { StyleSheet, View } from 'react-native';
-
+import { StyleSheet, View, Text, Button } from 'react-native';
 import useCachedResources from './hooks/useCachedResources';
-import BottomTabNavigator from './navigation/BottomTabNavigator';
-import LinkingConfiguration from './navigation/LinkingConfiguration';
 
 const Stack = createStackNavigator();
+
+function HomeScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
+    </View>
+  );
+}
+
+function DetailsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+      <Button
+        title="Go to Details... again"
+        onPress={() => navigation.push('Details')}
+      />
+      <Button title="Go to Home" onPress={() => navigation.navigate('Home')}/>
+      <Button title="Go back" onPress={() => navigation.goBack()}/>
+      <Button
+        title="Go back to first screen in stack"
+        onPress={() => navigation.popToTop()}
+      />
+    </View>
+  );
+}
+
 
 export default function App(props) {
   const isLoadingComplete = useCachedResources();
@@ -17,14 +44,12 @@ export default function App(props) {
     return null;
   } else {
     return (
-      <View style={styles.container}>
-        <NavigationContainer linking={LinkingConfiguration}>
-          <Stack.Navigator>
-            <Stack.Screen name="Root" component={BottomTabNavigator} />
-          </Stack.Navigator>
-        </NavigationContainer>
-        <StatusBar style="auto" />
-      </View>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Details" component={DetailsScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     );
   }
 }
@@ -32,6 +57,28 @@ export default function App(props) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
   },
 });
+
+
+
+
+
+
+
+
+
+{/*
+import { StatusBar } from 'expo-status-bar';
+import BottomTabNavigator from './navigation/BottomTabNavigator';
+import LinkingConfiguration from './navigation/LinkingConfiguration';  
+<View style={styles.container}>
+<NavigationContainer linking={LinkingConfiguration}>
+  <Stack.Navigator>
+    <Stack.Screen name="Root" component={BottomTabNavigator} />
+  </Stack.Navigator>
+</NavigationContainer>
+<StatusBar style="auto" />
+</View> */}
