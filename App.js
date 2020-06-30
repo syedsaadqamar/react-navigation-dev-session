b99c206501a54ec7b1d55f1abd080399d40fcdf5
Saad <syed.saad@iomechs.com>
2020-07-01 00:43:52 +0500

    Add style in button

--- App.js
+++ App.js
@@ -9,7 +9,7 @@ const Stack = createStackNavigator();
 
 function HomeScreen({ navigation, route }) {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <View style={styles.container}>
       <Text>Home Screen</Text>
       {route?.params?.itemId && <Text>Got data {route.params.itemId}</Text>}
       <Button
@@ -31,21 +31,29 @@ function DetailsScreen({ route, navigation }) {
   const { itemId } = route.params;
   const { otherParam } = route.params;
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <View style={styles.container}>
       <Text>Details Screen</Text>
       <Text>itemId: {JSON.stringify(itemId)}</Text>
-      <Text>otherParam: {JSON.stringify(otherParam)}</Text>
-      <Button
-        title="Go to Details... again"
-        onPress={() =>
-          navigation.push('Details', {
-            itemId: Math.floor(Math.random() * 100),
-          })
-        }
-      />
-      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
-      <Button title="Go back" onPress={() => navigation.goBack()} />
-      <Button title="Go to previous screen with params" onPress={() => navigation.navigate('Home', { itemId })} />
+      <Text style={styles.buttonStyle}>otherParam: {JSON.stringify(otherParam)}</Text>
+      <View style={styles.buttonStyle}>
+        <Button
+          title="Go to Details... again"
+          onPress={() =>
+            navigation.push('Details', {
+              itemId: Math.floor(Math.random() * 100),
+            })
+          }
+        />
+      </View>
+      <View style={styles.buttonStyle}>
+        <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+      </View>
+      <View style={styles.buttonStyle}>
+        <Button title="Go back" onPress={() => navigation.goBack()} />
+      </View>
+      <View style={styles.buttonStyle}>
+        <Button title="Go to previous screen with params" onPress={() => navigation.navigate('Home', { itemId })} />
+      </View>
     </View>
   );
 }
@@ -58,7 +66,7 @@ export default function App(props) {
   } else {
     return (
       <NavigationContainer>
-        <Stack.Navigator /** screenOptions={({ route }) => ({...HeaderOpts.header(route)})}*/ >
+        <Stack.Navigator screenOptions={({ route }) => ({...HeaderOpts.header(route)})} >
           <Stack.Screen
             name="Home"
             component={HomeScreen}
@@ -68,7 +76,7 @@ export default function App(props) {
               //   backgroundColor: '#f4511e',
               // },
               // headerTintColor: '#fff',
-              ////  ...HeaderOpts.header({name: 'Home'})
+              //// ...HeaderOpts.header({name: 'Home'})
               //// headerShown: false
             }}
           />
@@ -89,6 +97,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center'
   },
+  buttonStyle: {
+    marginBottom: 10
+  }
 });
 
 
